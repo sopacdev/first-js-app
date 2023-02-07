9b87b569a15915c0b828507566f4be15c0041003
sopacdev <jarreau.scanlan@gmail.com>
2023-02-07 12:18:11 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 pokemonList:[
     {name: "Pikachu", height: 5, type: ["electricity", "ground"]},
     {name: "Squirtle", height: 6, type: ["water", "ground"]},
-    {name: "Bulbasaur", height: 7, type: ["grass", "poison"]}
+    {name: "Bulbasaur", height: 7, type: ["grass", "poison", "ground"]}
 ]
