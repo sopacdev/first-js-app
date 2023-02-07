0a9aea28e33ae66d324c2c7fa8ff211a3946ebb4
sopacdev <jarreau.scanlan@gmail.com>
2023-02-07 12:15:17 -0800

    Update scripts.js
    deleted original intro code and wrote new code for 3 pokemon including nested arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert( 'Hello World' )
-let favoriteFood = 'Smashburger'
-document.write(favoriteFood)
+pokemonList:[
+    {name: "Pikachu", height: 5, type: ["electricity", "ground"]},
+    {name: "Squirtle", height: 6, type: ["water", "ground"]},
+    {name: "Bulbasaur", height: 7, type: ["grass", "poison"]}
+]
