b9a97c1bd7dccbfc6cd7890584052f300c3196e8
sopacdev <jarreau.scanlan@gmail.com>
2023-02-21 11:09:08 -0800

    Update scripts.js
    created a condition to state "wow thats big" for any pokemon over height of 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
-pokemonList:[
+let pokemonList = [
     {name: "Pikachu", height: 5, type: ["electricity", "ground"]},
     {name: "Squirtle", height: 6, type: ["water", "ground"]},
     {name: "Bulbasaur", height: 7, type: ["grass", "poison", "ground"]}
-]
+];
+for (let i=0; i<pokemonList.length; i++){
+    if (pokemonList[i].height>6){
+    document.write(pokemonList[i].name + "(Height:" + pokemonList[i].height + ") - Wow, That's big!") 
+} else {
+    document.write(pokemonList[i].name + "(Height:" + pokemonList[i].height + ")")
+ }
+}
